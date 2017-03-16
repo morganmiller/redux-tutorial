bb5d9576ab32148b4778bf32187f12c0a2570961
Morgan Miller <m.miller722@gmail.com>
2017-03-16 17:35:51 -0600

    refactors components to use jsx

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
-        MyTitleFactory({ title: 'lol', color: 'papayawhip' }),
-        MyTitleFactory({ title: 'ok', color: 'rebeccapurple' }),
-        MyTitleFactory({ title: 'fine', color: 'darkvioletred' })
-      )
+      <div>
+        <MyTitle title="yoooo" color="rebeccapurple" />
+        <MyTitle title="hellooo" color="cyan" />
+        <MyTitle title="hiya" color="thistle" />
+      </div>
     )
   }
 })
