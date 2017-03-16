0f4e1ce753395a1cec3524a914c958a836743373
Morgan Miller <m.miller722@gmail.com>
2017-03-16 16:26:50 -0600

    adds low-level example for creating multiple elements from one component

---
+++ js/ClientApp.js
@@ -0,0 +1,29 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1(null, "checkout this component")
+      )
+    )
+  }
+})
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle)
+      )
+    )
+  }
+});
+
+ReactDOM.render(React.createElement(MyFirstComponent),
+  document.getElementById('app')
+);
