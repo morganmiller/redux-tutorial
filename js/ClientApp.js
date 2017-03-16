c134faa7235aad538ea6ae3d3088c14b5144525e
Morgan Miller <m.miller722@gmail.com>
2017-03-16 17:53:35 -0600

    incorporates styling for streaming app

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MyTitle from './MyTitle'
+import { render } from 'react-dom'
+import '../public/normalize.css'
+import '../public/style.css'
 
-var MyFirstComponent = React.createClass({
-  render: function () {
+const App = React.createClass({
+  render () {
     return (
-      <div>
-        <MyTitle title="yoooo" color="rebeccapurple" />
-        <MyTitle title="hellooo" color="cyan" />
-        <MyTitle title="hiya" color="thistle" />
+      <div className='app'>
+        <div className='landing'>
+          <h1>video streaming thing</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse All</a>
+        </div>
       </div>
     )
   }
 })
 
-ReactDOM.render(React.createElement(MyFirstComponent),
-  document.getElementById('app')
-)
+render(<App />, document.getElementById('app'))
