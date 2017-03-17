8d5a8b653f835f6561d641044b3953e861999d50
Morgan Miller <m.miller722@gmail.com>
2017-03-16 19:09:13 -0600

    adds header to search

--- js/Search.js
+++ js/Search.js
@@ -6,9 +6,15 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (<ShowCard {...show} key={show.imdbID} />)
-        })}
+        <header>
+          <h1>video streaming thing</h1>
+          <input type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (<ShowCard {...show} key={show.imdbID} />)
+          })}
+        </div>
       </div>
     )
   }
