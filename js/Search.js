b6b2f7558be8376cc2eb2320fa11dfee3be8d6df
Morgan Miller <m.miller722@gmail.com>
2017-03-16 19:16:13 -0600

    adds search bar state to header

--- js/Search.js
+++ js/Search.js
@@ -3,12 +3,20 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
         <header>
           <h1>video streaming thing</h1>
-          <input type='text' placeholder='Search' />
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
           {preload.shows.map((show) => {
