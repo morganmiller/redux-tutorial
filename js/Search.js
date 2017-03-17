7b8e28165d695f8890edb2968a70b1d071f21996
Morgan Miller <m.miller722@gmail.com>
2017-03-16 18:52:16 -0600

    adds showcards for browsing shows

--- js/Search.js
+++ js/Search.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import preload from '../public/data.json'
+import ShowCard from './ShowCard'
 
 const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+        {preload.shows.map((show) => {
+          return (<ShowCard show={show} />)
+        })}
       </div>
     )
   }
