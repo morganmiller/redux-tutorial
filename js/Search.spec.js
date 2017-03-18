2d9c7c33f50165d1ddf637f672df573a3ee20055
Morgan Miller <m.miller722@gmail.com>
2017-03-18 11:03:41 -0600

    tests showcard search

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Search from './Search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
 
@@ -7,4 +9,9 @@ test('Search snapshot test', () => {
   const component = shallow(<Search />)
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
