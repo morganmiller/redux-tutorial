2650d8622f84389cfeec52d4d0a10b6e3334dead
Morgan Miller <m.miller722@gmail.com>
2017-03-20 10:28:27 -0600

    makes showcards clickable

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 const { string } = React.PropTypes
+import { Link } from 'react-router'
 
 const ShowCard = React.createClass({
   propTypes: { // propTypes is only used as a development tool, are not checked in production
@@ -11,14 +12,16 @@ const ShowCard = React.createClass({
   render () {
     const { poster, title, year, description } = this.props // destructuring
     return (
-      <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`} />
-        <div>
-          <h3>{title}</h3>
-          <h4>({year})</h4>
-          <p>{description}</p>
+      <Link to={`/details/${this.props.imdbID}`}>
+        <div className='show-card'>
+          <img src={`/public/img/posters/${poster}`} />
+          <div>
+            <h3>{title}</h3>
+            <h4>({year})</h4>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 })
