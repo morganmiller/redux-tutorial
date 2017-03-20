0b9a39b18fe0ac0249584626857e09de0fe6a66f
Morgan Miller <m.miller722@gmail.com>
2017-03-20 16:59:54 -0600

    uses props as search term value and updates searchTerm state via reducer

--- js/Landing.js
+++ js/Landing.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-const { string } = React.PropTypes
+import { setSearchTerm } from './actionCreators'
+const { string, func } = React.PropTypes
 
 const Landing = React.createClass({
   propTypes: {
-    searchTerm: string
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    // props.dispatch comes from invocation of connect()
+    this.props.dispatch(setSearchTerm(event.target.value))
   },
   render () {
     return (
       <div className='landing'>
         <h1>video streaming thing</h1>
-        <input value={this.props.searchTerm} type='text' placeholder='Search' />
+        <input value={this.props.searchTerm}
+          type='text'
+          placeholder='Search'
+          onChange={this.handleSearchTermChange}
+        />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
