6d27d58266cad89c979f96045dfed3e496665e31
Morgan Miller <m.miller722@gmail.com>
2017-03-20 17:11:01 -0600

    submits searchTerm from Landing back to store, not yet picked up by Search

--- js/Landing.js
+++ js/Landing.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { setSearchTerm } from './actionCreators'
-const { string, func } = React.PropTypes
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
+  contextTypes: {
+    router: object
+  },
   propTypes: {
     searchTerm: string,
     dispatch: func
@@ -13,15 +16,21 @@ const Landing = React.createClass({
     // props.dispatch comes from invocation of connect()
     this.props.dispatch(setSearchTerm(event.target.value))
   },
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    this.context.router.transitionTo('/search')
+  },
   render () {
     return (
       <div className='landing'>
         <h1>video streaming thing</h1>
-        <input value={this.props.searchTerm}
-          type='text'
-          placeholder='Search'
-          onChange={this.handleSearchTermChange}
-        />
+        <form onSubmit={this.handleSearchSubmit}>
+          <input value={this.props.searchTerm}
+            type='text'
+            placeholder='Search'
+            onChange={this.handleSearchTermChange}
+          />
+        </form>
         <Link to='/search'>or Browse All</Link>
       </div>
     )
