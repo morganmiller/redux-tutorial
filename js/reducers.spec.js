03f02367d530f9274a3f4c92d1c305e07b28d521
Morgan Miller <m.miller722@gmail.com>
2017-03-20 18:45:59 -0600

    classifies reducer test better

--- js/reducers.spec.js
+++ js/reducers.spec.js
@@ -1,6 +1,6 @@
 import reducers from './reducers'
 
-test('reducers', () => {
+test('reducers: SET_SEARCH_TERM', () => {
   let state
   state = reducers({ searchTerm: '', omdbData: {} }, { type: 'SET_SEARCH_TERM', searchTerm: 'house' })
   expect(state).toEqual({ searchTerm: 'house', omdbData: {} })
