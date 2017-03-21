5ac81d2cfc61cc12c0dc815b8c1a6f220f2a4481
Morgan Miller <m.miller722@gmail.com>
2017-03-20 18:16:13 -0600

    fetches imdb rating with async actions

--- js/reducers.js
+++ js/reducers.js
@@ -1,7 +1,8 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
 const DEFAULT_STATE = {
-  searchTerm: ''
+  searchTerm: '',
+  omdbData: {}
 }
 
 const setSearchTerm = (state, action) => {
@@ -11,12 +12,22 @@ const setSearchTerm = (state, action) => {
   // return Object.assign({}, state, {searchTerm: action.searchTerm}) (works?)
 }
 
+const addOMDBData = (state, action) => {
+  const newOMDBData = {}
+  Object.assign(newOMDBData, state.omdbData, {[action.imdbID]: action.omdbData})
+  const newState = {}
+  Object.assign(newState, state, {omdbData: newOMDBData})
+  return newState
+}
+
 // check out 'combine reducers' as alternative
 // investigate normalization in redux
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return setSearchTerm(state, action)
+    case ADD_OMDB_DATA:
+      return addOMDBData(state, action)
     default:
       return state
   }
